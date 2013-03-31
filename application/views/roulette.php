b42664701b0031c7b2f25768b2e0f85b16cd30cd
Jean-Francois Gagne Berube <seozus@gmail.com>
2013-03-30 23:51:34 -0400

    Intégration de Google Analytics

--- application/views/roulette.php
+++ application/views/roulette.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>GeekRoulette.ca - l'endroit par excellence pour vos blind-dates geeks et hétéros</title>
+	<title>GeekRoulette.ca - L'endroit par excellence pour vos blind-dates geeks et hétéros</title>
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
 	<link rel="stylesheet"  type="text/css" href="<?=base_url()?>/assets/css/style.css">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
@@ -18,7 +18,7 @@
 		<div id="center" style="position:relative;">
             <!--Fenêtre de discussion-->
             <div id="frame_conversation">
-                <p>Bienvenue sur GeekRoulette.ca, l'endroit par excellence pour vos blind-dates geeks et hétéros. Nous savons que les geeks aiment beaucoup leur orinateur et qu'il est parfois difficile de décrocher et se faire des amis dans la vraie vie, Geekroulette est là. </p><p>À propos du service:</p>
+                <p style="margin-bottom:10px;">Bienvenue sur GeekRoulette.ca, l'endroit par excellence pour vos blind-dates geeks et hétéros, comme quoi ça peut être facile pour un geek de se faire de nouveaux amis.</p><p>À propos du service:</p>
                 <ul>
                     <li>Il est défendu de montrer sa bizoune.</li>
                     <li>Les invocations sataniques sont interdites les lundis et jeudis.</li>
@@ -75,6 +75,18 @@
     <script src="/assets/js/bootstrap.min.js"></script>
     <script type="text/JavaScript" src="<?=base_url()?>/assets/js/functions.js"></script>
     <script type="text/JavaScript" src="<?=base_url()?>/assets/js/chat.js"></script>
+	<script type="text/javascript">
 
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-39743030-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
 </body>
 </html>
